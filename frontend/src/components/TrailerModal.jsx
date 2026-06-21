def2b82e100394aac1ef5a59ffbaf4bd2a56ac95
nafisshahriar98 <nafis.shahriar045@gmail.com>
2026-06-21 18:28:38 +0600

    fixed trailer watching

--- frontend/src/components/TrailerModal.jsx
+++ frontend/src/components/TrailerModal.jsx
@@ -1,26 +1,27 @@
-  import { createPortal } from 'react-dom';
-  import ReactPlayer from 'react-player';
-  import '../css/TrailerModal.css'
+import { createPortal } from 'react-dom';
+import ReactPlayer from 'react-player';
+import '../css/TrailerModal.css'
 
-  function TrailerModal({ youtubeKey, onClose }) {
-      return createPortal(
-          <div className="modal-overlay" onClick={onClose}>
-              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                  <button className="modal-close" onClick={onClose}>✕</button>
-                  <div className="player-wrapper">
-                      <ReactPlayer
-                          url={`https://www.youtube.com/watch?v=${youtubeKey}`}
-                          playing={true}
-                          controls={true}
-                          width="100%"
-                          height="100%"
-                          style={{ position: 'absolute', top: 0, left: 0 }}
-                      />
-                  </div>
-              </div>
-          </div>,
-          document.body
-      )
-  }
+function TrailerModal({ youtubeKey, onClose }) {
+    return createPortal(
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <button className="modal-close" onClick={onClose}>✕</button>
+                <div className="player-wrapper">
+                    <ReactPlayer
+                        src={`https://www.youtube.com/watch?v=${youtubeKey}`}
+                        playing={true}
+                        muted={true}
+                        controls={true}
+                        width="100%"
+                        height="100%"
+                        style={{ position: 'absolute', top: 0, left: 0 }}
+                    />
+                </div>
+            </div>
+        </div>,
+        document.body
+    )
+}
 
-  export default TrailerModal
+export default TrailerModal
